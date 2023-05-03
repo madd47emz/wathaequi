c03c55654d88446fcd908c1ffb64f0219d3d5172
Mohamed Emziane <m.emziane@esi-sba.dz>
2023-05-04 00:20:01 +0100

    upgrade ms-gestion-document to v2 with security features

---
+++ ms-gestion-document-v2/src/main/java/com/example/msgestiondocumentv2/Entity/Paper.java
@@ -0,0 +1,30 @@
+package com.example.msgestiondocumentv2.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.sql.Date;
+import java.util.List;
+
+@Data @NoArgsConstructor @AllArgsConstructor
+@Document("papers")
+public class Paper {
+    @Id
+    private String id;
+    private String name;
+    private String type;
+    private Attachement file;
+
+    List<Attachement> folder;
+
+    private Date created;
+    private Date expiration;
+
+    private String userId;
+    private String agentId;
+    private String demandeId;
+    private String OfficeId;
+}
