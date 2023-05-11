87430fe58948affad56fa3611db7da4b52bc959e
dalmi20 <f.kessaissia@esi-sba.dz>
2023-05-11 15:28:58 +0100

    resolve axios problem

--- ms_auth_admin/src/main/java/com/example/auth/controller/UserController.java
+++ ms_auth_admin/src/main/java/com/example/auth/controller/UserController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+
 @RequestMapping("/users")
 @RestController
 public class UserController {
