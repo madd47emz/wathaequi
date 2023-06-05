c5e92d7277e9a4ff01a0be02d07842f66667be1c
Mohamed Emziane <m.emziane@esi-sba.dz>
2023-06-05 10:16:31 +0100

    'cleaning'

--- ms-gestion-demande-document/src/main/java/com/example/msgestiondemandedocuments_v1/repositories/DemandeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.msgestiondemandedocuments_v1.repositories;
-
-import com.example.msgestiondemandedocuments_v1.entity.Demande;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DemandeRepository extends JpaRepository<Demande,Long> {
-    //List<Demande> findByDocuments_Id(Long documentId);
-}
